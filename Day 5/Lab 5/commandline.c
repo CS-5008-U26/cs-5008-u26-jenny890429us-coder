4fb6344550c2b345374a53afb739edfa398aac09
Zhuo Wang <jenny890429us@gmail.com>
2026-06-27 13:18:43 -0700

    Fix Lab 5 commandline file path

--- Day 5/Lab 5/commandline.c
+++ Day 5/Lab 5/commandline.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-// For printf, fopen, fgets, and fclose.
+
 
 int main(int argc, char **argv)
 // argc is the number of command line arguments.
 // argv stores the command line arguments.
 {
     FILE *filePointer;
-    // Create a file pointer.
+    // File pointer used to open and read the file.
 
     char inputBuffer[200];
-    // Create a buffer to store one line from the file.
+    // Buffer to store one line from the file.
 
     if (argc <= 1)
-    // If there is no file name,
+    // If there is no file name on the command line,
     {
         return 0;
         // Do nothing.
     }
 
     filePointer = fopen(argv[1], "r");
-    // Open the file in read mode.
+    // Open the file name from argv[1].
 
     if (filePointer == NULL)
     // If the file cannot be opened,
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     }
 
     while (fgets(inputBuffer, 200, filePointer) != NULL)
-    // Read one line from the file until the end.
+    // Read one line at a time until the end of the file.
     {
         printf("%s", inputBuffer);
         // Print the line.
